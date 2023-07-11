e02601c5c2d86fadf9c76543256cc6e51f65d5ec
satheeshsimha <satheeshsk@hotmail.com>
2023-07-11 13:42:10 +0530

    more changes

--- codes/e2e_sim/navicsim.py
+++ codes/e2e_sim/navicsim.py
@@ -1,5 +1,7 @@
 import numpy as np
 from fractions import Fraction
+from sk_dsp_comm import fec_conv as fec     #pip/pip3 install scikit-dsp-comm
+import sk_dsp_comm.digitalcom as dc
 
 # CA code generation API
 #Initial condition of register G2 taken from NavIC ICD
@@ -116,6 +118,69 @@ def genNavicCaTable(samplingFreq):
     indexArr = indexArr.astype(int)
     return np.array([genNavicCaCode(i) for i in range(1,prnIdMax+1)])[:,indexArr].T     # column-wise PRN Sequence for all 14 satellites
 
+#function to compute crc-24q parity
+def rtk_crc24q(buff, length):
+    """Build the CRC Code
+
+    buff (contains the subframe bits right aligned i.e. additional zeros padded in first byte)\n
+    For ex: If the bits are 1101010001111001010100100111 (28 bits), they are stored in buff as\n
+    00001101 | 01000111 | 10010101 | 00100111\n
+    b0   |    b1    |    b2    |    b3
+    
+    :param list buff: navdata
+    :param int length: length of navdata
+    :returns list crc: crc-24Q parity
+    
+    """    
+    if length%8!=0:
+        zero = 8-(length%8)
+        buff = np.concatenate((np.zeros(zero,dtype=np.uint8),buff))
+        length+=zero
+    
+    packed_data = np.packbits(buff.reshape(-1,8))
+    crc = 0
+    
+    
+    for i in range(int(length/8)):
+        crc = ((crc << 8) & 0xFFFFFF) ^ tbl_CRC24Q[(crc >> 16) ^ packed_data[i]]
+
+    return crc
+
+tbl_CRC24Q = [
+    0x000000,0x864CFB,0x8AD50D,0x0C99F6,0x93E6E1,0x15AA1A,0x1933EC,0x9F7F17,
+    0xA18139,0x27CDC2,0x2B5434,0xAD18CF,0x3267D8,0xB42B23,0xB8B2D5,0x3EFE2E,
+    0xC54E89,0x430272,0x4F9B84,0xC9D77F,0x56A868,0xD0E493,0xDC7D65,0x5A319E,
+    0x64CFB0,0xE2834B,0xEE1ABD,0x685646,0xF72951,0x7165AA,0x7DFC5C,0xFBB0A7,
+    0x0CD1E9,0x8A9D12,0x8604E4,0x00481F,0x9F3708,0x197BF3,0x15E205,0x93AEFE,
+    0xAD50D0,0x2B1C2B,0x2785DD,0xA1C926,0x3EB631,0xB8FACA,0xB4633C,0x322FC7,
+    0xC99F60,0x4FD39B,0x434A6D,0xC50696,0x5A7981,0xDC357A,0xD0AC8C,0x56E077,
+    0x681E59,0xEE52A2,0xE2CB54,0x6487AF,0xFBF8B8,0x7DB443,0x712DB5,0xF7614E,
+    0x19A3D2,0x9FEF29,0x9376DF,0x153A24,0x8A4533,0x0C09C8,0x00903E,0x86DCC5,
+    0xB822EB,0x3E6E10,0x32F7E6,0xB4BB1D,0x2BC40A,0xAD88F1,0xA11107,0x275DFC,
+    0xDCED5B,0x5AA1A0,0x563856,0xD074AD,0x4F0BBA,0xC94741,0xC5DEB7,0x43924C,
+    0x7D6C62,0xFB2099,0xF7B96F,0x71F594,0xEE8A83,0x68C678,0x645F8E,0xE21375,
+    0x15723B,0x933EC0,0x9FA736,0x19EBCD,0x8694DA,0x00D821,0x0C41D7,0x8A0D2C,
+    0xB4F302,0x32BFF9,0x3E260F,0xB86AF4,0x2715E3,0xA15918,0xADC0EE,0x2B8C15,
+    0xD03CB2,0x567049,0x5AE9BF,0xDCA544,0x43DA53,0xC596A8,0xC90F5E,0x4F43A5,
+    0x71BD8B,0xF7F170,0xFB6886,0x7D247D,0xE25B6A,0x641791,0x688E67,0xEEC29C,
+    0x3347A4,0xB50B5F,0xB992A9,0x3FDE52,0xA0A145,0x26EDBE,0x2A7448,0xAC38B3,
+    0x92C69D,0x148A66,0x181390,0x9E5F6B,0x01207C,0x876C87,0x8BF571,0x0DB98A,
+    0xF6092D,0x7045D6,0x7CDC20,0xFA90DB,0x65EFCC,0xE3A337,0xEF3AC1,0x69763A,
+    0x578814,0xD1C4EF,0xDD5D19,0x5B11E2,0xC46EF5,0x42220E,0x4EBBF8,0xC8F703,
+    0x3F964D,0xB9DAB6,0xB54340,0x330FBB,0xAC70AC,0x2A3C57,0x26A5A1,0xA0E95A,
+    0x9E1774,0x185B8F,0x14C279,0x928E82,0x0DF195,0x8BBD6E,0x872498,0x016863,
+    0xFAD8C4,0x7C943F,0x700DC9,0xF64132,0x693E25,0xEF72DE,0xE3EB28,0x65A7D3,
+    0x5B59FD,0xDD1506,0xD18CF0,0x57C00B,0xC8BF1C,0x4EF3E7,0x426A11,0xC426EA,
+    0x2AE476,0xACA88D,0xA0317B,0x267D80,0xB90297,0x3F4E6C,0x33D79A,0xB59B61,
+    0x8B654F,0x0D29B4,0x01B042,0x87FCB9,0x1883AE,0x9ECF55,0x9256A3,0x141A58,
+    0xEFAAFF,0x69E604,0x657FF2,0xE33309,0x7C4C1E,0xFA00E5,0xF69913,0x70D5E8,
+    0x4E2BC6,0xC8673D,0xC4FECB,0x42B230,0xDDCD27,0x5B81DC,0x57182A,0xD154D1,
+    0x26359F,0xA07964,0xACE092,0x2AAC69,0xB5D37E,0x339F85,0x3F0673,0xB94A88,
+    0x87B4A6,0x01F85D,0x0D61AB,0x8B2D50,0x145247,0x921EBC,0x9E874A,0x18CBB1,
+    0xE37B16,0x6537ED,0x69AE1B,0xEFE2E0,0x709DF7,0xF6D10C,0xFA48FA,0x7C0401,
+    0x42FA2F,0xC4B6D4,0xC82F22,0x4E63D9,0xD11CCE,0x575035,0x5BC9C3,0xDD8538 ]
+
+
 class NavicL5sModulator():
     """NavicL5sModulator will generate modulated IQ samples
     
@@ -206,21 +271,36 @@ class NavicDataGen():
     """
     
     def __init__(self, ds=50, fs=10*1.023e6, numChannel=1, file=None):
-      """The init function is executed always when class is initiated
-      
-      :param int ds: data rate/bit rate
-      :param int fs: sample rate
-      :param int numChannel: number of Channels
-      :param bool file: data file if present
+        """The init function is executed always when class is initiated
 
-      """
-      self.dataRate = ds
-      self.sampleRate = fs
-      self.numSamplesPerBit = round(fs/ds)
-      self.samplesToNextBit = self.numSamplesPerBit
-      self.numChannel = numChannel
-      self.bitStream = np.empty((1, numChannel))
-      self.bitStream[0,:] = np.random.binomial(1, 0.5, (numChannel, ))
+        :param int ds: data rate/bit rate
+        :param int fs: sample rate
+        :param int numChannel: number of Channels
+        :param bool file: data file if present
+
+        """
+        self.dataRate = ds
+        self.sampleRate = fs
+        self.numSymbolsPerFrame = 2400
+        self.numSymbolsPerSubFrame = 600
+        self.numDataBitsPerSubFrame = 292
+        self.numSamplesPerBit = round(fs/ds)
+        self.samplesToNextBit = self.numSamplesPerBit
+        self.numChannel = numChannel
+        self.bitcnt = 0
+
+        self.bitStream = np.empty((0, numChannel))
+        self.datStream = np.empty((0, numChannel))
+        full_frame = np.empty(0)
+        full_data = np.empty(0)
+        for i in range(numChannel):
+            frame, data = self.__frameGen()
+            full_frame = np.append(full_frame,frame)
+            full_data = np.append(full_data,data)
+
+        self.bitStream = np.append(self.bitStream,full_frame.reshape(numChannel,-1).T,axis=0)
+        self.datStream = np.append(self.datStream,full_data.reshape(numChannel,-1).T,axis=0)
+
 
     def GenerateBits(self, timeInterval):
       """Function to generate bits upto given time interval
@@ -238,15 +318,27 @@ class NavicDataGen():
         # If remaining samples to generate is within the current bit's remaining duration
         if(bufferCnt < self.samplesToNextBit):
           # Copy current bit
-          genStream = np.append(genStream, np.repeat(self.bitStream[-1:], bufferCnt, axis=0), axis=0)
+          genStream = np.append(genStream, np.repeat(self.bitStream[self.bitcnt: self.bitcnt+1,:], bufferCnt, axis=0), axis=0)
           # Update current bit's remaining duration
           self.samplesToNextBit -= bufferCnt
+          # End loop
           bufferCnt = -1
         else:
           # Copy current bit for remaining duration
-          genStream = np.append(genStream, np.repeat(self.bitStream[-1:], self.samplesToNextBit, axis=0), axis=0)
-          # Generate new bit
-          self.bitStream = np.append(self.bitStream, np.random.binomial(1, 0.5, (1, self.numChannel)), axis=0)
+          genStream = np.append(genStream, np.repeat(self.bitStream[self.bitcnt: self.bitcnt+1,:], self.samplesToNextBit, axis=0), axis=0)
+          # Increment bit counter
+          self.bitcnt+=1
+          # If current frame ended, generate new frame
+          if(self.bitcnt%self.numSymbolsPerFrame==0):
+            full_frame = np.empty(0)
+            full_data = np.empty(0)
+            for i in range(self.numChannel):
+                frame, data = self.__frameGen()
+                full_frame = np.append(full_frame,frame)
+                full_data = np.append(full_data,data)
+
+            self.bitStream = np.append(self.bitStream,full_frame.reshape(self.numChannel,-1).T,axis=0)
+            self.datStream = np.append(self.datStream,full_data.reshape(self.numChannel,-1).T,axis=0)
           # Update remaining samples to generate
           bufferCnt -= self.samplesToNextBit
           # Update remaining duration of current bit
@@ -254,13 +346,58 @@ class NavicDataGen():
       
       return genStream[1:numBitsToGen+1]
     
+    def __frameGen(self):
+        """Function to add CRC, tail bits, interleave and encode the data bits
+
+        :returns list frame: encoded symbols 
+        :returns list nav_data: pre-encoded data
+
+        """
+        nav_data = np.array([])
+        frame = np.array([],dtype=int)
+        for i in range(4): 
+            data = np.array([np.random.randint(0, 2) for _ in range(262)])
+            cr = rtk_crc24q(data, len(data))
+            crc = "{:06X}".format(cr)
+            binary = bin(int(crc,16))[2:]
+            padded_binary = list(binary.zfill(len(crc) * 4))
+            nav_crc = np.append(data,padded_binary)
+            nav_crc = np.array([int(bit) for bit in nav_crc])
+
+        #fec encoding and tail bits
+            nav_crc_tail = np.append(nav_crc,np.zeros(6)) #tail bits
+            nav_data = np.append(nav_data,nav_crc_tail)
+            state = '000000'
+            cc1 = fec.FECConv(('1111001','1011011'))
+            nav_encd,state = cc1.conv_encoder(nav_crc_tail,state)
+
+        #interleaving
+            k=8
+            n= 73
+            interleave = lambda x,k,n: x.reshape(n,-1).T.flatten()
+            nav_intrlv = interleave(nav_encd,k,n)
+
+        #adding sync word EB90 Hex
+            sync_hex = 'EB90'
+            sync_bin = bin(int(sync_hex,16))[2:]
+            sync_bin = [int(bit) for bit in sync_bin]
+            encsubframe = np.append(sync_bin,nav_intrlv)
+            frame = np.append(frame,encsubframe)
+
+        return frame, nav_data
+
+
     def GetBitStream(self):
        """Function to return bitstream of nav data
              
        :returns list genStream: generated bits
 
        """
-       return self.bitStream
+       numBitsGen = (self.bitcnt//self.numSymbolsPerSubFrame)*self.numDataBitsPerSubFrame
+       remain = self.bitcnt%self.numSymbolsPerSubFrame
+       if(remain>=16):
+          numBitsGen += (remain-16)//2
+       return self.datStream[0:numBitsGen]
       
 # Channel model API
 #the functions below simulate a channel, thereby create offsets and shift delays.
@@ -719,3 +856,100 @@ class NavicTracker:
         self.pDLLWPrevious1 = 0
         self.pDLLNCOOut = 0
 #end of acquisition and tracking code
+
+#function for bit synchronization index
+def gnss_bit_synchronize(data, n):
+    """Bit synchronization for IRNSS receiver
+    
+    :param list data: samples from tracking loop
+    :param int n: samples per bit
+    :returns int syncidx: returns the bit starting index
+    
+    """
+    # Input validation
+    numdata = len(data)
+    if not isinstance(data, (np.ndarray, list)):
+        raise ValueError("Invalid input type for 'data'. Expected ndarray or list.")
+    if not isinstance(n, (int, np.integer)) or n <= 0:
+        raise ValueError("Invalid input type or value for 'n'. Expected positive integer.")
+    if numdata < n:
+        raise ValueError("Invalid input length. 'data' should have length greater than or equal to 'n'.")
+
+    # Find the transition locations in the data by finding the difference of signs in adjacent data
+    all_signs = np.concatenate(([1], np.sign(data)))
+    diff_vals = np.diff(all_signs)
+    transition_flags = diff_vals != 0
+
+    # Find the number of transitions at each data location
+    ntemp = np.uint16(n)
+    num_avg_samples = np.uint64(np.floor(numdata/ntemp) * ntemp)
+    trmat = np.reshape(transition_flags[:num_avg_samples], (-1, ntemp)).T
+
+    # Consolidate the number of transitions at each sample location
+    numtr = np.sum(trmat, axis=1)
+
+    # Find the starting location corresponding to maximum transitions and cast the value
+    syncidx = np.argmax(numtr, axis=0)
+    syncidx = np.uint16(syncidx)
+
+    return syncidx
+
+#function for frame synchronization index
+SYNC_WORD = [1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0]  # Sync word value
+INV_SYNC_WORD = [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1] # Inverted Sync word value (to check in case of inverted bits)
+def find_sync_word(data):
+    """Frame synchronization for IRNSS receiver
+    
+    :param list data: samples from tracking loop
+    :returns int status: indicator for presence of sync word
+    :returns int fsync_index: returns the frame starting index
+    
+    """
+    sync_word_length = len(SYNC_WORD)
+    window = []
+    fsync_index = -1
+    status = 0
+
+    for index, bit in enumerate(data):
+        window.append(bit)
+        if len(window) > sync_word_length:
+            window.pop(0)
+        if window == SYNC_WORD:
+            status = 1
+            fsync_index = index - sync_word_length + 1
+        elif window == INV_SYNC_WORD:
+            status = -1
+            fsync_index = index - sync_word_length + 1
+
+    return status, fsync_index
+
+def decoder(subframe, num_sf):
+    """Function decodes the subframes
+    :param list subframe: subframes from the extracted frame after frame synchronization
+    :param int num_sf: number of suframes found
+    :return list decd_total: returns decoded nav_data
+
+    """
+    decd_total = np.empty(0)
+    cc1 = fec.FECConv(('1111001','1011011'))
+    #remove sync word
+    for buff in subframe:
+        rec_subframe = buff[16:]
+
+        k = 8
+        n = 73
+
+    #undo interleaving
+        deinterleave = lambda x,k,n: x.reshape(k,-1).T.flatten()
+        nav_deintrv = deinterleave(rec_subframe,k,n)
+        nav_deintrv = [int(bit) for bit in nav_deintrv]
+        nav_deintrv = np.asarray(nav_deintrv)
+        nav_deintrv = np.append(nav_deintrv,np.zeros(6)) #to nullify error in viterbi decoder
+
+
+    #fec decoding and ber
+        yn_hard = ((np.sign(nav_deintrv.real)+1)/2).astype(int)
+        nav_decd = cc1.viterbi_decoder(yn_hard,'hard')
+        decd_total = np.append(decd_total,nav_decd) 
+
+    return decd_total
